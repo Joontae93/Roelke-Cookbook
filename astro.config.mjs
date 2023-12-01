3f75e831172c1b9c088adbe047d0eb5ea2e03ca7
KJ Roelke <kj.roelke@gmail.com>
2023-11-30 20:20:58 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kjroelke/github.io',
+  site: 'https://kjroelke.github.io',
   base: 'roelke-cookbook',
   integrations: [react()]
 });
